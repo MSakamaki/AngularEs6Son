7029603b29d9e6a71e4e406b4b2ba83b0a6d5980
joe-re <joe.tialtngo@gmail.com>
2015-03-07 14:25:45 +0900

    リスト画面から新規登録画面に遷移できることを保証した

--- e2e/sample.spec.js
+++ e2e/sample.spec.js
@@ -11,4 +11,22 @@ describe('/',()=> {
       expect(addBeansBtn.isPresent()).toBe(true);
     });
   });
+
+  it('リスト画面から新規登録画面に遷移できること', ()=> {
+    //TODO: URL ハードコードを設定ファイルにまとめる
+    browser.get('http://localhost:9000');
+
+    var addBeansBtn = element(by.id('add-button'));
+    var submit = element(by.id('submit'));
+
+    browser.wait(()=> {
+      return addBeansBtn.isPresent();
+    }, 50000).then(()=> {
+      return addBeansBtn.click();
+    }).then(()=> {
+      return submit.isPresent();
+    }).then((isPresent)=> {
+      expect(submit.isPresent()).toBe(true);
+    });
+  });
 });
