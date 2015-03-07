062fba8b85e70c8913cc1183bebf64266098d060
Kuniwak <yuki.kokubun@mixi.co.jp>
2015-03-07 12:45:58 +0900

    /にアクセスしたとき、ユーザーは豆を追加できることを保証した

--- e2e/sample.spec.js
+++ e2e/sample.spec.js
@@ -1,12 +1,14 @@
-describe('protractor sample',()=> {
+describe('/',()=> {
+  it('新規登録ボタンが表示されている', ()=> {
+    //TODO: URL ハードコードを設定ファイルにまとめる
+    browser.get('http://localhost:9000');
 
-  it('list done', ()=> {
-    browser.get('http://localhost:9000/');
-    var member1 = element(by.repeater('member in list.members').row(0));
-    browser.wait(()=>{
-      return member1.isPresent();
-    }, 10000, 'about add cart').then(()=>{
-      expect(member1.getText()).toEqual('ID：1 名前：鈴木 一郎 登録日：2015/01/02 更新日：2015/01/01 所属：Japan 編集 削除');
+    var addBeansBtn = element(by.id('add-button'));
+
+    browser.wait(()=> {
+      return addBeansBtn.isPresent();
+    }, 50000).then(()=> {
+      expect(addBeansBtn.isPresent()).toBe(true);
     });
   });
 });
